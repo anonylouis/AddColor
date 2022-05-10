002d2d1d34af8c58955d7c550da8e2a3f3167333
Louis CALVIE <lcalvie@e1r9p6.42.fr>
2022-05-10 15:35:02 +0200

    basic color and style

---
+++ color.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcalvie <lcalvie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/07 13:14:51 by lcalvie           #+#    #+#             */
+/*   Updated: 2022/05/07 13:20:27 by lcalvie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef COLOR_HPP
+# define COLOR_HPP
+
+# define reset "\033[00m"
+
+# define italic "\033[3m"
+# define bold "\033[1m"
+# define underline "\033[4m"
+
+# define white "\033[37m"
+# define red "\033[31m"
+# define green "\033[32m"
+# define blue "\033[34m"
+
+#endif
